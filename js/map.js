8c057754d19c33bd03faf67227142d660b66e992
eufanzky <eufanzky@gmail.com>
2023-08-02 19:23:44 -0400

    FIXING satellite styles and footer styles

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 let satelliteIcon = L.icon({
-  iconUrl: "../assets/satellite.png",
+  iconUrl: "../assets/Satellite.svg",
   //shadowUrl: 'leaf-shadow.png',
 
   iconSize: [38, 45], // size of the icon
@@ -12,6 +12,12 @@ let satelliteIcon = L.icon({
   iconAnchor: [22, 44], // point of the icon which will correspond to marker's location
   //shadowAnchor: [4, 62],  // the same for the shadow
   popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
+let satelliteIcon2 = L.icon({
+  iconUrl: "../assets/Satellite2.svg",
+  iconSize: [38, 45],
+  iconAnchor: [22, 44],
+  popupAnchor: [-3, -76],
 });
 let marker = L.marker([0, 0], { icon: satelliteIcon }).addTo(map);
 
@@ -71,12 +77,39 @@ function handleThemes(event) {
   if (currentMapLayer) {
     currentMapLayer.remove();
   }
-
+  const pageTitle = document.querySelector(".title");
   if (selectedThemeValue === "1") {
+    pageTitle.style.color = "var(--black)";
     currentMapLayer = mapStyle1.addTo(map);
+    marker.setIcon(satelliteIcon);
   } else if (selectedThemeValue === "2") {
+    pageTitle.style.color = "var(--baby-powder)";
     currentMapLayer = mapStyle2.addTo(map);
+    marker.setIcon(satelliteIcon2);
   } else if (selectedThemeValue === "3") {
+    pageTitle.style.color = "var(--baby-powder)";
     currentMapLayer = mapStyle3.addTo(map);
+    marker.setIcon(satelliteIcon2);
   }
 }
+
+// function cambiarColorSVG(color) {
+//   fetch("../assets/Satellite.js") // Reemplaza 'ruta/a/mi_archivo.svg' con la URL de tu archivo SVG
+//     .then((response) => response.text())
+//     .then((svgData) => {
+//       const parser = new DOMParser();
+//       const svgDocument = parser.parseFromString(svgData, "image/svg+xml");
+//       const svgElement = svgDocument.documentElement;
+
+//       svgElement.setAttribute("fill", color);
+
+//       const svgContainer = document.getElementById("svgContainer");
+//       svgContainer.innerHTML = "";
+//       svgContainer.appendChild(svgElement);
+//     })
+//     .catch((error) => {
+//       console.error("Error al cargar el archivo SVG:", error);
+//     });
+// }
+
+// cambiarColorSVG("red");
